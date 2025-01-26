e12eef79fc2621da1c75650770f73c3f6729c05c
Your GitHub Username <sagarbadal70@gmail.com>
2025-01-26 16:21:28 +0530

    redis deployment

--- src/helpers/cache.ts
+++ src/helpers/cache.ts
@@ -9,7 +9,7 @@ dotenv.config();
 // console.log(process.env.redisUrl);
 //mongoose orm modification to embed redis in it
 // modify redis url according
-const redisUrl = process.env.redisUrl || "redis://127.0.0.1:6379";
+const redisUrl = process.env.redisUrl;
 export const client = createClient({
   url: redisUrl,
 });
